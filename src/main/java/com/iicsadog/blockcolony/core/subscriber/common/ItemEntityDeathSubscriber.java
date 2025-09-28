64a07d1bd8b1ceb051715e1066a7895782c91b09
sxt <2735950909@qq.com>
2025-09-28 23:20:17 +0800

    修复了灵魂会突然下降的bug！感谢mojang的弱智双端同步机制！

--- src/main/java/com/iicsadog/blockcolony/core/subscriber/common/ItemEntityDeathSubscriber.java
+++ src/main/java/com/iicsadog/blockcolony/core/subscriber/common/ItemEntityDeathSubscriber.java
@@ -5,23 +5,13 @@ import com.iicsadog.blockcolony.BlockColony;
 import com.iicsadog.blockcolony.api.item.ModItems;
 import com.iicsadog.blockcolony.core.entity.RisingItemEntity;
 import com.iicsadog.blockcolony.core.event.common.ItemEntityDeathEvent;
-import com.mojang.logging.LogUtils;
-import java.io.IOException;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.tags.DamageTypeTags;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.damagesource.DamageType;
-import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import org.slf4j.Logger;
 
 /**
  * 物品实体死亡时间订阅器。
@@ -31,8 +21,6 @@ import org.slf4j.Logger;
  */
 @EventBusSubscriber(modid = BlockColony.MODID)
 public class ItemEntityDeathSubscriber {
-
-    private static final Logger logger = LogUtils.getLogger();
 
     /**
      * 焚烧灵魂沙，会得到一个冉冉升起的灵魂。
@@ -55,7 +43,6 @@ public class ItemEntityDeathSubscriber {
             level, e.getEntity().getX(), e.getEntity().getY(), e.getEntity().getZ(), soul
         );
         level.addFreshEntity(soulItemEntity);
-        soulItemEntity.setDeltaMovement(0, 0.1, 0);
     }
 
 }
