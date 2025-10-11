28efdbce6af7fef6bbdc15aa3311d8abb26a1a22
sxt <2735950909@qq.com>
2025-10-11 20:06:13 +0800

    为魂龛方块添加朝向逻辑，完善语言文件、状态文件，并更新未实现特性列表。

--- src/main/java/com/iicsadog/blocksblocks/core/block/SoulNicheBlock.java
+++ src/main/java/com/iicsadog/blocksblocks/core/block/SoulNicheBlock.java
@@ -3,13 +3,20 @@ package com.iicsadog.blocksblocks.core.block;
 import com.iicsadog.blocksblocks.core.block.entity.SoulNicheBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
@@ -24,7 +31,12 @@ import org.jetbrains.annotations.Nullable;
  */
 public class SoulNicheBlock extends BaseEntityBlock {
 
-    public static final VoxelShape SHAPE = Block.box(1.0D, 0.0D, 4.0D, 15.0D, 14.0D, 13.0D);
+    public static final VoxelShape SHAPE_NORTH = Block.box(1.0D, 0.0D, 4.0D, 15.0D, 14.0D, 13.0D);
+    public static final VoxelShape SHAPE_SOUTH = Block.box(1.0D, 0.0D, 3.0D, 15.0D, 14.0D, 12.0D);
+    public static final VoxelShape SHAPE_WEST = Block.box(4.0D, 0.0D, 1.0D, 13.0D, 14.0D, 15.0D);
+    public static final VoxelShape SHAPE_EAST = Block.box(3.0D, 0.0D, 1.0D, 12.0D, 14.0D, 15.0D);
+
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     /**
      * SoulNicheBlock 的构造方法，用于初始化一个新的魂龛（Soul Niche）方块实例。
@@ -34,11 +46,17 @@ public class SoulNicheBlock extends BaseEntityBlock {
      */
     public SoulNicheBlock() {
         super(Properties.ofFullCopy(Blocks.OAK_PLANKS));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     protected MapCodec<? extends BaseEntityBlock> codec() {
         return null;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Nullable
@@ -57,6 +75,30 @@ public class SoulNicheBlock extends BaseEntityBlock {
     @NotNull
     protected VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos,
                                   @NotNull CollisionContext context) {
-        return SHAPE;
+        return switch (state.getValue(FACING)) {
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            case EAST -> SHAPE_EAST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Override
+    @NotNull
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    @NotNull
+    @Deprecated
+    protected BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 }
