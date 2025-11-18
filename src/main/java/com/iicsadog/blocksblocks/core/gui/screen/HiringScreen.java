efec73a38e19f0ba95e7d4f2d498fa14e9d9cc44
sxt <2735950909@qq.com>
2025-11-18 14:23:03 +0800

    添加了按钮的名称显示，暂时没添加中文。

--- src/main/java/com/iicsadog/blocksblocks/core/gui/screen/HiringScreen.java
+++ src/main/java/com/iicsadog/blocksblocks/core/gui/screen/HiringScreen.java
@@ -6,6 +6,7 @@ import com.iicsadog.blocksblocks.core.network.vo.EmployeeVO;
 import io.wispforest.owo.ui.base.BaseUIModelScreen;
 import io.wispforest.owo.ui.container.FlowLayout;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -17,6 +18,10 @@ import java.util.UUID;
  * @since 2025/11/17
  */
 public class HiringScreen extends BaseUIModelScreen<FlowLayout> {
+    private static final String HIRE = "gui.blocks_blocks.hiring.hire";
+    private static final String FIRE = "gui.blocks_blocks.hiring.fire";
+    private static final String TRANSFER = "gui.blocks_blocks.hiring.transfer";
+
     private final List<EmployeeVO> vos = new ArrayList<>();
     private final UUID buildingId;
 
@@ -51,10 +56,20 @@ public class HiringScreen extends BaseUIModelScreen<FlowLayout> {
     @Override
     protected void build(FlowLayout rootComponent) {
         List<FlowLayout> info = this.vos.stream()
-            .map(vo -> this.model.expandTemplate(FlowLayout.class, "info", Map.of(
-                "name", vo.name(),
-                "status", "Hire"
-            ))).toList();
+            .map(vo -> {
+                Map<String, String> params = new HashMap<>();
+                String status;
+                if (buildingId.equals(vo.workFor())) {
+                    status = FIRE;
+                } else if (vo.workFor() == null) {
+                    status = HIRE;
+                } else {
+                    status = TRANSFER;
+                }
+                params.put("name", vo.name());
+                params.put("status", status);
+                return this.model.expandTemplate(FlowLayout.class, "info", params);
+            }).toList();
         rootComponent.childById(FlowLayout.class, "info-container").children(info);
     }
 
